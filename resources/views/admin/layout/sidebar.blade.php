beebcbc2175263006457e5ee0bb21f097e4ebcc9
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2020-12-14 12:36:02 +0600

    lang update by moon

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -9,7 +9,7 @@
         <li class="sidebar-header">{{ __('ADMIN DASHBOARD') }} </li>
         <li>
             <a href="{{ route('admin.dashboard.index') }}" class="waves-effect">
-                <i class="icon-home"></i> <span>Dashboard</span>
+                <i class="icon-home"></i> <span>{{ __('Dashboard') }}</span>
             </a>
         </li>
         <li>
@@ -25,54 +25,54 @@
         <li>
             <a href="javaScript:void();" class="waves-effect">
                 <i class="icon-briefcase"></i>
-                <span>Worker | Service</span> <i class="fa fa-angle-left pull-right"></i>
+                <span>{{ __('Worker | Service') }}</span> <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.worker-service-category.index') }}"><i class="fa fa-circle-o"></i> Category</a></li>
-                <li><a href="{{ route('admin.worker-service.index') }}"><i class="fa fa-circle-o"></i> Service</a></li>
+                <li><a href="{{ route('admin.worker-service-category.index') }}"><i class="fa fa-circle-o"></i>{{ __(' Category') }}</a></li>
+                <li><a href="{{ route('admin.worker-service.index') }}"><i class="fa fa-circle-o"></i> {{ __('Service') }}</a></li>
             </ul>
         </li>
         <li>
             <a href="javaScript:void();" class="waves-effect">
                 <i class="icon-briefcase"></i>
-                <span>Member. | Service</span> <i class="fa fa-angle-left pull-right"></i>
+                <span>{{ __('Member. | Service') }}</span> <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.membership-service-category.index') }}"><i class="fa fa-circle-o"></i> Category</a></li>
-                <li><a href="{{ route('admin.membership-service.index') }}"><i class="fa fa-circle-o"></i> Service</a></li>
+                <li><a href="{{ route('admin.membership-service-category.index') }}"><i class="fa fa-circle-o"></i> {{ __('Category') }}</a></li>
+                <li><a href="{{ route('admin.membership-service.index') }}"><i class="fa fa-circle-o"></i> {{ __('Service') }}</a></li>
             </ul>
         </li>
         <li>
             <a href="javaScript:void();" class="waves-effect">
                 <i class="icon-briefcase"></i>
-                <span>Notices</span> <i class="fa fa-angle-left pull-right"></i>
+                <span>{{ __('Notices') }}</span> <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.admin-notice.index') }}"><i class="fa fa-circle-o"></i> Admin notices</a></li>
+                <li><a href="{{ route('admin.admin-notice.index') }}"><i class="fa fa-circle-o"></i> {{ __('Admin notices') }}</a></li>
             </ul>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.controller-notice.index') }}"><i class="fa fa-circle-o"></i> Controller notices</a></li>
+                <li><a href="{{ route('admin.controller-notice.index') }}"><i class="fa fa-circle-o"></i> {{ __('Controller notices') }}</a></li>
             </ul>
         </li>
         <li>
             <a href="javaScript:void();" class="waves-effect">
                 <i class="icon-briefcase"></i>
-                <span>Ads.</span> <i class="fa fa-angle-left pull-right"></i>
+                <span>{{ __('Ads.') }}</span> <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.admin-ads.index') }}"><i class="fa fa-circle-o"></i> Admin ads.</a></li>
+                <li><a href="{{ route('admin.admin-ads.index') }}"><i class="fa fa-circle-o"></i> {{ __('Admin ads') }}.</a></li>
             </ul>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.controller-ads.index') }}"><i class="fa fa-circle-o"></i> Controller ads.</a></li>
+                <li><a href="{{ route('admin.controller-ads.index') }}"><i class="fa fa-circle-o"></i> {{ __('Controller ads') }}.</a></li>
             </ul>
         </li>
         <li>
             <a href="javaScript:void();" class="waves-effect">
                 <i class="icon-briefcase"></i>
-                <span>Membership</span> <i class="fa fa-angle-left pull-right"></i>
+                <span>{{ __('Membership') }}</span> <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.membership-package.index') }}"><i class="fa fa-circle-o"></i> Memberships</a></li>
+                <li><a href="{{ route('admin.membership-package.index') }}"><i class="fa fa-circle-o"></i> {{ __('Memberships') }}</a></li>
             </ul>
         </li>
 
@@ -80,25 +80,30 @@
         <li>
             <a href="javaScript:void();" class="waves-effect">
                 <i class="icon-briefcase"></i>
-                <span>More</span> <i class="fa fa-angle-left pull-right"></i>
+                <span>{{ __('More') }}</span> <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.showOffer') }}"><i class="fa fa-circle-o"></i>Offer</a></li>
+                <li><a href="{{ route('admin.showOffer') }}"><i class="fa fa-circle-o"></i>{{ __('Offer') }}</a></li>
             </ul>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.showReferralIncome') }}"><i class="fa fa-circle-o"></i> Referral Income System</a></li>
+                <li><a href="{{ route('admin.showReferralIncome') }}"><i class="fa fa-circle-o"></i> {{ __('Referral Income System') }}</a></li>
             </ul>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.showVideoTraining') }}"><i class="fa fa-circle-o"></i> Video Training</a></li>
+                <li><a href="{{ route('admin.showVideoTraining') }}"><i class="fa fa-circle-o"></i> {{ __('Video Training') }}</a></li>
             </ul>
             <ul class="sidebar-submenu">
-                <li><a href="{{ route('admin.showHelpLine') }}"><i class="fa fa-circle-o"></i> Help Line</a></li>
+                <li><a href="{{ route('admin.showHelpLine') }}"><i class="fa fa-circle-o"></i>{{ __(' Help Line') }}</a></li>
             </ul>
         </li>
-        <li><a href="{{ url('/language-management/bn/translations') }}" target="_blank" class="waves-effect"><i class="fa fa-circle-o text-red"></i> <span>Translation</span></a></li>
-        <li><a href="{{ route('admin.showAbout') }}" class="waves-effect"><i class="fa fa-circle-o text-red"></i> <span>About</span></a></li>
-        <li><a href="{{ route('admin.showFaq') }}" class="waves-effect"><i class="fa fa-circle-o text-yellow"></i> <span>FAQ</span></a></li>
-        <li><a href="{{ route('admin.showTermsAndCondition') }}" class="waves-effect"><i class="fa fa-circle-o text-aqua"></i> <span>Terms & Conditions</span></a></li>
-        <li><a href="{{ route('admin.showPrivacyPolicy') }}" class="waves-effect"><i class="fa fa-circle-o text-aqua"></i> <span>Privacy Policy</span></a></li>
+        <li><a href="{{ url('/language-management/bn/translations') }}" target="_blank" class="waves-effect"><i class="fa fa-circle-o text-red"></i>
+                <span>{{ __('Translation') }}</span></a></li>
+        <li><a href="{{ route('admin.showAbout') }}" class="waves-effect"><i class="fa fa-circle-o text-red"></i>
+                <span>{{ __('About') }}</span></a></li>
+        <li><a href="{{ route('admin.showFaq') }}" class="waves-effect"><i class="fa fa-circle-o text-yellow"></i>
+                <span>{{ __('FAQ') }}</span></a></li>
+        <li><a href="{{ route('admin.showTermsAndCondition') }}" class="waves-effect"><i class="fa fa-circle-o text-aqua"></i>
+                <span>{{ __('Terms & Conditions') }}</span></a></li>
+        <li><a href="{{ route('admin.showPrivacyPolicy') }}" class="waves-effect"><i class="fa fa-circle-o text-aqua"></i>
+                <span>{{ __('Privacy Policy') }}</span></a></li>
     </ul>
 </div>
