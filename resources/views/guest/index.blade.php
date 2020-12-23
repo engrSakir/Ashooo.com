f93a761d5636697b100b8b21a175671632fa9952
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2020-12-23 14:45:19 +0600

    introduction page update

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -11,8 +11,8 @@
                             <img src="{{ asset( get_static_option('logo')  ?? 'uploads/images/defaults/logo.png') }}" alt="" class="mx-100 my-5">
                             <div class="row">
                                 <div class="container mb-5">
-                                    <h4>{{ __('Welcome to digital world') }}</h4>
-                                    <p>{{ __('Best service marketplace in bangladesh.') }}</p>
+                                    <h3 class="text-success"><b>{{ __('Service on Demand') }}</b></h3>
+                                    <p>{{ __('Top & Best Service Marketplace in Bangladesh') }}</p>
                                 </div>
                             </div>
                         </div>
@@ -23,14 +23,27 @@
         <!-- Swiper intro ends -->
 
         <!-- login buttons -->
-        <div class="row mx-0 bottom-button-container">
-            <div class="col">
-                <a href="{{ route('login') }}" class="btn btn-default btn-lg btn-rounded shadow btn-block">{{ __('Login') }}</a>
+        <div class="row mx-0">
+            <div class="col-12 container text-center small">
+                <p>{{ __('সার্ভিস নিতে অথবা সার্ভিস দিতে সাইন-ইন করুন। আপনার কোনো অ্যাকাউন্ট নেই? এখনই সাইন আপ করুন।') }}</p>
+                <br>
             </div>
             <div class="col">
-                <a href="{{ route('register') }}" class="btn btn-white bg-white btn-lg btn-rounded shadow btn-block">{{ __('Register') }}</a>
+                <a href="{{ route('login') }}" class="btn btn-default btn-lg btn-rounded bg-success shadow btn-block">{{ __('Sign in') }}</a>
+            </div>
+            <div class="col">
+                <a href="{{ route('register') }}" class="btn btn-white bg-white btn-lg btn-rounded shadow btn-outline-success btn-block text-success">{{ __('Sign up') }}</a>
             </div>
         </div>
         <!-- login buttons -->
+        <hr class="bg-success">
+        <div class="row mx-0 container">
+            <div class="container text-center mb-5">
+                <h3 class="btn btn-default btn-lg btn-rounded shadow my-2"><b>{{ __('লোকাল সার্ভিস অন ডিমান্ড') }}</b></h3>
+            </div>
+            
+        </div>
+        <!-- login buttons -->
+        <hr>
     </div>
 @endsection
